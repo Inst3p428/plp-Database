2db04a4d57ba37161461e17f24150f08c62ba94d
Inst3p428 <rotichallank@gmail.com>
2025-05-05 00:04:26 +0300

    answers.sql

--- week six/answers.sql
+++ week six/answers.sql
@@ -2,8 +2,12 @@
 --Write an SQL query to get the firstName, lastName, email, and officeCode of all employees.
 --Use an INNER JOIN to combine the employees table with the offices table using the officeCode column.
 
-SELECT firstName,lastName,email,officeCode 
-FROM employees INNER JOIN offices
+SELECT employee.firstName,  -- Employee's first name
+    employees.lastName,     -- Employee's last name
+    employees.email,        -- Employee's email address
+    employees.officeCode    -- Code of the office the employee works in
+FROM employees 
+INNER JOIN offices
 ON employees.officeCode = offices.officeCode;
 
 
@@ -12,8 +16,11 @@ ON employees.officeCode = offices.officeCode;
 --Write an SQL query to get the productName, productVendor, and productLine from the products table.
 --Use a LEFT JOIN to combine the products table with the productlines table using the productLine column.
 
-SELECT productName,productVendor,productLine
-FROM products LEFT JOIN productlines
+SELECT  products.productName,        -- Name of the product
+        products.productVendor,      -- Vendor supplying the product
+        products.productLine         -- Product line category
+FROM products 
+LEFT JOIN productlines
 ON products.productLine = productlines.productLine;
 
 
@@ -21,7 +28,10 @@ ON products.productLine = productlines.productLine;
 --Write an SQL query to retrieve the orderDate, shippedDate, status, and customerNumber for the first 10 orders.
 --Use a RIGHT JOIN to combine the customers table with the orders table using the customerNumber column.
 
-SELECT orderDate, shippedDate, status, customerNumber
-LIMIT 10
+SELECT orders.orderDate -- Date when the order was placed
+       orders.shippedDate,          -- Date when the order was shipped
+       orders.status,               -- Current status of the order
+       orders.customerNumber        -- Identifier of the customer who placed the order
 FROM customers RIGHT JOIN orders
-ON customers.customerNumber = orders.customerNumber;
+ON customers.customerNumber = orders.customerNumber
+LIMIT 10;
